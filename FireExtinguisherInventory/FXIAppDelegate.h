21deed06fbe5e7979ebcfd5b120c453392fec13e
sellers04 <norredm@rodgersm-mbproi7.pp.oregonstate.edu>
2014-07-18 09:15:52 -0700

    Initial Commit

---
+++ FireExtinguisherInventory/FXIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FXIAppDelegate.h
+//  FireExtinguisherInventory
+//
+//  Created by norredm on 7/18/14.
+//  Copyright (c) 2014 oregonstate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FXIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FireExtinguisherInventoryTests/FireExtinguisherInventoryTests.m
@@ -0,0 +1,34 @@
+//
+//  FireExtinguisherInventoryTests.m
+//  FireExtinguisherInventoryTests
+//
+//  Created by norredm on 7/18/14.
+//  Copyright (c) 2014 oregonstate. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FireExtinguisherInventoryTests : XCTestCase
+
+@end
+
+@implementation FireExtinguisherInventoryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
